4fc387251985c29c33b9d5cc91ed52b39253b367
Mahan Noorbahr <m2.noorbahr@gmail.com>
2020-12-15 03:30:06 +0800

    Continued developing the UI

--- Source/app/src/main/java/com/mahan/randommoviespinner/MainActivity.java
+++ Source/app/src/main/java/com/mahan/randommoviespinner/MainActivity.java
@@ -4,12 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
 
     private Button netflixgBtn, disneyplusBtn, appleBtn, huluBtn, amazonBtn, hboBtn;
+    private Spinner genreSpinner,imdbSpinner;
+    private CheckBox movieCB, showCB;
+
+
+    private boolean[] chosenServices;
+    private int totalServicesSelected;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +39,140 @@ public class MainActivity extends AppCompatActivity {
         amazonBtn = findViewById(R.id.amazonBtn);
         hboBtn = findViewById(R.id.hboBtn);
 
+        chosenServices = new boolean[] {true,false,false,false,false,false};
+        totalServicesSelected = 1;
+
+        genreSpinner = findViewById(R.id.genreSpinner);
+        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_genres,
+                R.layout.colored_spinner_layout);
+        arrayAdapter.setDropDownViewResource(R.layout.colored_spinner_dropwdown_layout);
+        genreSpinner.setAdapter(arrayAdapter);
+        genreSpinner.setOnItemSelectedListener(this);
+
+
+        movieCB = findViewById(R.id.movieCB);
+        movieCB.setChecked(true);
+        movieCB.setOnCheckedChangeListener(this);
+
+        showCB = findViewById(R.id.showCB);
+        showCB.setChecked(true);
+        showCB.setOnCheckedChangeListener(this);
+
+        imdbSpinner = findViewById(R.id.imdbSpinner);
+        ArrayAdapter arrayAdapter2 = ArrayAdapter.createFromResource(this,
+                R.array.imdbScore,
+                R.layout.colored_spinner_layout);
+        arrayAdapter2.setDropDownViewResource(R.layout.colored_spinner_dropwdown_layout);
+        imdbSpinner.setAdapter(arrayAdapter2);
+        imdbSpinner.setOnItemSelectedListener(this);
+
+
+
+
+
     }
 
     public void onServiceClick(View view){
 
+        System.out.println(totalServicesSelected);
+
         switch (view.getId()){
             case R.id.netflixBtn:
-                Toast.makeText(this, "NETFLIX PRESSED", Toast.LENGTH_SHORT).show();
+                if(chosenServices[0] && totalServicesSelected > 1) {
+                    netflixgBtn.setBackgroundResource(R.drawable.ic_netflix_disabled);
+                    chosenServices[0] = false;
+                    totalServicesSelected -= 1;
+                }
+                else if (!chosenServices[0]){
+                    netflixgBtn.setBackgroundResource(R.drawable.ic_netflix);
+                    chosenServices[0] = true;
+                    totalServicesSelected += 1;
+                }
                 break;
             case R.id.disneyBtn:
-                Toast.makeText(this, "DISNEY PRESSED", Toast.LENGTH_SHORT).show();
+                if(chosenServices[1] && totalServicesSelected > 1) {
+                    disneyplusBtn.setBackgroundResource(R.drawable.ic_disney_plus_disable);
+                    chosenServices[1] = false;
+                    totalServicesSelected -= 1;
+                }
+                else if (!chosenServices[1]) {
+                    disneyplusBtn.setBackgroundResource(R.drawable.ic_disney_plus);
+                    chosenServices[1] = true;
+                    totalServicesSelected += 1;
+                }
                 break;
             case R.id.appleBtn:
-                Toast.makeText(this, "APPLE PRESSED", Toast.LENGTH_SHORT).show();
+                if(chosenServices[2] && totalServicesSelected > 1) {
+                    appleBtn.setBackgroundResource(R.drawable.ic_apple_tv_plus_disable);
+                    chosenServices[2] = false;
+                    totalServicesSelected -= 1;
+                }
+                else if (!chosenServices[2]) {
+                    appleBtn.setBackgroundResource(R.drawable.ic_apple_tv_plus);
+                    chosenServices[2] = true;
+                    totalServicesSelected += 1;
+                }
                 break;
             case R.id.huluBtn:
-                Toast.makeText(this, "HULU PRESSED", Toast.LENGTH_SHORT).show();
+                if(chosenServices[3] && totalServicesSelected > 1) {
+                    huluBtn.setBackgroundResource(R.drawable.ic_hulu_disable);
+                    chosenServices[3] = false;
+                    totalServicesSelected -= 1;
+                }
+                else if (!chosenServices[3]) {
+                    huluBtn.setBackgroundResource(R.drawable.ic_hulu);
+                    chosenServices[3] = true;
+                    totalServicesSelected += 1;
+                }
                 break;
             case R.id.amazonBtn:
-                Toast.makeText(this, "AMAZON PRIME PRESSED", Toast.LENGTH_SHORT).show();
+                if(chosenServices[4] && totalServicesSelected > 1) {
+                    amazonBtn.setBackgroundResource(R.drawable.ic_amazon_prime_disable);
+                    chosenServices[4] = false;
+                    totalServicesSelected -= 1;
+                }
+                else if (!chosenServices[4]) {
+                    amazonBtn.setBackgroundResource(R.drawable.ic_amazon_prime);
+                    chosenServices[4] = true;
+                    totalServicesSelected += 1;
+                }
                 break;
             case R.id.hboBtn:
-                Toast.makeText(this, "HBO MAX PRESSED", Toast.LENGTH_SHORT).show();
+                if(chosenServices[5] && totalServicesSelected > 1) {
+                    hboBtn.setBackgroundResource(R.drawable.ic_hbo_max_disable);
+                    chosenServices[5] = false;
+                    totalServicesSelected -= 1;
+                }
+                else if (!chosenServices[5]) {
+                    hboBtn.setBackgroundResource(R.drawable.ic_hbo_max);
+                    chosenServices[5] = true;
+                    totalServicesSelected += 1;
+                }
                 break;
+        }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this,parent.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(!isChecked){
+            if(buttonView.getId() == R.id.movieCB && !showCB.isChecked()){
+                movieCB.setChecked(true);
+            }
+            else if(buttonView.getId() == R.id.showCB && !movieCB.isChecked()){
+                showCB.setChecked(true);
+            }
         }
 
     }
